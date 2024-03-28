9d172a36d0395cce255e7f1e8b5f0fb6e5c5d910
Vincent Trinh <gawplayid154@gmail.com>
2024-03-27 22:27:05 -0400

    add view classes

--- banksystem/api/views.py
+++ banksystem/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import *
+from .serializers import *
+
+class BranchesAPIView(generics.ListCreateAPIView):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+
+class BranchDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+
+class BanksAPIView(generics.ListCreateAPIView):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
